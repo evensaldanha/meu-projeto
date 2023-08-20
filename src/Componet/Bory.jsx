68fe6acd954d12264b849a181caec2476e4727db
Even Saldanha <evensaldanhass@gmail.com>
2023-08-20 13:29:23 -0300

    atualizacao de nomes

--- src/Componet/Bory.jsx
+++ src/Componet/Bory.jsx
@@ -2,37 +2,40 @@ import React, { useState } from 'react'
 import "../style.css"
 
 export default function Bory() {
-const [name,setName]= useState("");
+  const [name, setName] = useState("");
 
   return (
     <div className='container'>
-      <label htmlFor='name' className='nome'>Nome:
+      <label htmlFor='name' className='name'>Nome:
         <input type="text"
           required
           name='name'
-          value={name || ""}
+          value={name}
           onChange={e => setName(e.target.value)}
-          />
+          placeholder='Nome'
+        />
         <button type='submit'
-        onClick={() => setName([...name,name], setName(""))}>Enviar</button>
+          onClick={() => setName([...name, name], setName(""))}>Enviar</button>
       </label>
       <label className='email'
-      htmlFor='email'>
+        htmlFor='email'>
         Email:
         <input type='email'
           required
-          name='name'/>
+          name='name'
+          placeholder='Email' />
         <button type='submit'>Enviar</button>
       </label>
       <label className='telephone'
-      htmlFor='telephone'>
+        htmlFor='telephone'>
         Telefone:
         <input type="tel" maxLength={11}
-          required />
-        <button type='submit' 
+          required
+          placeholder=' Telefone' />
+        <button type='submit'
         >Enviar</button>
       </label>
-      <p>{name}</p>
+      <div>{name}</div>
       {/* {newName.map(nomes => nomes)} */}
     </div>
   )
