809f47ec88803aa9d02510a422bc3fdd0cd7ee3b
Even Saldanha <evensaldanhass@gmail.com>
2023-09-05 22:24:15 -0300

    comp. home

---
+++ src/Componet/Home.jsx
@@ -0,0 +1,13 @@
+// import Eu from "../imagrnd/Eu.jpg"
+import home from "../Componet/home.css"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Bem vindo!</h1>
+            <p>Me chamo Even Saldanha, você está conhecendo meu primeiro projeto em react.
+                Este projeto foi desenvolvido por mim para colocar em prática o que estou estudando. </p>
+            <p>Obrigada pela visita!</p>
+        </div>
+    )
+}           
