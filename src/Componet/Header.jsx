a48b69adb68b7ed27ab9211505837bf9c5bd8a58
Even Saldanha <evensaldanhass@gmail.com>
2023-09-12 12:24:30 -0300

    tirei o link do cadastro pq n fez sentido no modal

--- src/Componet/Header.jsx
+++ src/Componet/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link} from "react-router-dom";
 import {FaInstagram, FaLinkedin} from "react-icons/fa"
 import "./header.css"
+import Register from '../Register';
 
 export default function Header() {
+    const[Open,setOpen]= useState(false);
+
+
   return (
     <div className='link'>
         <Link style={{textDecoration:" none"}} to="/home">Home</Link>
         <Link style={{textDecoration:" none"}} to="/contact">Contato</Link>
+        <button 
+        className='button'
+        onClick={()=> setOpen(!Open)}>Cadastro</button>
+        <Register isOpen={Open} setOpen={setOpen}></Register>
         <a href='https://www.instagram.com/evensaldanha/'><FaInstagram/> </a>
         <a  href='https://www.linkedin.com/in/even-saldanha-b1936b226/' ><FaLinkedin/> </a>
-        <Link to="/cadastro">Cadastro</Link>
+   
     </div>
   )
 }
