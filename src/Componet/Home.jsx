458ca6bed93233b0d51fee60bb87c4fa0193afbb
Even Saldanha <evensaldanhass@gmail.com>
2023-09-16 00:50:37 -0300

    utilizai funcao setInterval,useState.

--- src/Componet/Home.jsx
+++ src/Componet/Home.jsx
@@ -1,7 +1,33 @@
 // import Eu from "../imagrnd/Eu.jpg"
+import { useState } from "react"
 import home from "../Componet/home.css"
 
 export default function Home() {
+    const [cor,setcor]= useState(1);
+
+    const preto={color:"#000"};
+    const cinza={color:"#acacac"};
+    const marron={color:"#965aas"}
+
+    const retCor=(c)=>{
+        if(cor==1){
+          return  preto
+        } else if(cor==2){
+            return cinza
+        }else{
+            return marron
+        }
+    }
+
+    const mudarCor=()=>{
+        setcor(cor+1)
+        if(cor>2){
+            setcor(1)
+        }
+    }
+
+    setInterval(mudarCor,3000)
+
     const times=()=>{
         const time=new Date().getHours()
   
@@ -16,7 +42,7 @@ export default function Home() {
 
     return (
         <div>
-            <h1>Bem vindo!</h1>
+            <h1 style={retCor(cor)}>Bem vindo!</h1>
             <div style={{display:"flex", justifyContent:"flex-start",alignItems:"flex-start"}}> {times()}</div>
             <p>Me chamo Even Saldanha, você está conhecendo meu primeiro projeto em react.
                 Este projeto foi desenvolvido por mim para colocar em prática o que estou estudando. </p>
