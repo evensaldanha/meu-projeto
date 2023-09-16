4daaa15e329119a6c7995ccce1c2a966d3745326
Even Saldanha <evensaldanhass@gmail.com>
2023-09-15 21:01:02 -0300

    utilizado condicao no cumprimento de boma dia...

--- src/Componet/Home.jsx
+++ src/Componet/Home.jsx
@@ -2,9 +2,22 @@
 import home from "../Componet/home.css"
 
 export default function Home() {
+    const times=()=>{
+        const time=new Date().getHours()
+  
+        if(time >= 0 && time < 13){
+          return <p>Bom dia</p>
+        } else if( time >= 13 && time < 18){
+          return <p>Boa tarde</p>
+        }else{
+           return <p>Boa noite</p>
+        }
+      }
+
     return (
         <div>
             <h1>Bem vindo!</h1>
+            <div style={{display:"flex", justifyContent:"flex-start",alignItems:"flex-start"}}> {times()}</div>
             <p>Me chamo Even Saldanha, você está conhecendo meu primeiro projeto em react.
                 Este projeto foi desenvolvido por mim para colocar em prática o que estou estudando. </p>
             <p>Obrigada pela visita!</p>
