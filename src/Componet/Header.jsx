01dec3a6a0cd0494c9345c0fe9415af6b08b0182
Even Saldanha <evensaldanhass@gmail.com>
2023-09-18 14:25:19 -0300

    func. para logar e deslogar com useState

--- src/Componet/Header.jsx
+++ src/Componet/Header.jsx
@@ -1,37 +1,36 @@
 import React, { useState } from 'react'
-import { Link} from "react-router-dom";
-import {FaInstagram, FaLinkedin} from "react-icons/fa"
+import { Link } from "react-router-dom";
+import { FaInstagram, FaLinkedin } from "react-icons/fa"
 import "./header.css"
 import Register from '../Register';
 
 export default function Header() {
-    const[Open,setOpen]= useState(false);
-    
+  const [Open, setOpen] = useState(false);
+  const [login, setLogin] = useState(false);
 
-    const times=()=>{
-      const time=new Date().getHours()
+  const logged = () => {
+    return "logado"
+  }
+  const logoff = () => {
+    return "Deslogado..."
+  }
 
-      if(time >= 0 && time < 13){
-        return <p>Bom dia</p>
-      } else if( time >= 13 && time < 18){
-        return <p>Boa tarde</p>
-      }else{
-         return <p>Boa noite</p>
-      }
-    }
 
   return (
     <div className='link'>
-       <div style={{display:"flex", justifyContent:"flex-start",alignItems:"flex-start"}}> {times()}</div>
-        <Link style={{textDecoration:" none"}} to="/home">Home</Link>
-        <Link style={{textDecoration:" none"}} to="/contact">Contato</Link>
-        <button 
+      <div className='div-log'>{login ? logged() : logoff()}</div>
+      <button onClick={() => setLogin(!login)}
+      className='botao-log'
+      >{login ? "sair" : "entrar"}</button>
+      <Link style={{ textDecoration: " none" }} to="/home">Home</Link>
+      <Link style={{ textDecoration: " none" }} to="/contact">Contato</Link>
+      <button
         className='button'
-        onClick={()=> setOpen(!Open)}>Cadastro</button>
-        <Register isOpen={Open} setOpen={setOpen}></Register>
-        <a href='https://www.instagram.com/evensaldanha/'><FaInstagram/> </a>
-        <a  href='https://www.linkedin.com/in/even-saldanha-b1936b226/' ><FaLinkedin/> </a>
-   
+        onClick={() => setOpen(!Open)}>Cadastro</button>
+      <Register isOpen={Open} setOpen={setOpen}></Register>
+      <a href='https://www.instagram.com/evensaldanha/'><FaInstagram /> </a>
+      <a href='https://www.linkedin.com/in/even-saldanha-b1936b226/' ><FaLinkedin /> </a>
+
     </div>
   )
 }       
